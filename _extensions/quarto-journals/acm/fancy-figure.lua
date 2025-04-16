c9f4fc0be13ff8ed3b7c29dd69cd526349f720ec
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-04-16 14:17:07 +0200

    Don't support \Description for fancy figure yet
    to mitigate and check that only #36 - Outer par mode error with Quarto 1.4  remains

--- _extensions/quarto-journals/acm/fancy-figure.lua
+++ _extensions/quarto-journals/acm/fancy-figure.lua
@@ -58,12 +58,13 @@ return {
       end
       description = pandoc.write(pandoc.Pandoc({description}), "latex")
       if quarto.doc.is_format("latex") then
-        local caption = pandoc.write(pandoc.Pandoc({image.caption}), "latex")
-        local blockStr = "\\begin{figure}\n" ..
-          "{\\centering \\includegraphics{" .. image.src .. "}}\n" ..
-          "\\caption{" .. caption .. "}\n\\Description{"
-          .. description .. "}\n\\end{figure}\n"
-        return {pandoc.RawInline("latex", blockStr)}
+        -- local caption = pandoc.write(pandoc.Pandoc({image.caption}), "latex")
+        -- local blockStr = "\\begin{figure}\n" ..
+        --  "{\\centering \\includegraphics{" .. image.src .. "}}\n" ..
+        --  "\\caption{" .. caption .. "}\n\\Description{"
+        --  .. description .. "}\n\\end{figure}\n"
+        -- return {pandoc.RawInline("latex", blockStr)}
+        return image
       else
         image.attr.attributes["alt"] = description
         image.attr.attributes["fig-alt"] = nil
